25a1b73af6f6db5fea2904b590563691c6913550
gubinfedor0707 <gubinfedor1998@gmail.com>
2025-05-26 17:12:52 +0300

    добавил cameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,13 @@ dependencies {
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.logging)
+
+//    CameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.mlkit.vision)
+    implementation(libs.androidx.camera.extensions)
 }
