4fbbf618ed3908ebf27122583670d84594e0b30a
Dmitrii.Skorodumov <skorodumov@skbkontur.ru>
2025-06-17 16:24:48 +0300

    counter_key вынес в константу

--- app/src/main/java/com/example/qrcodeapp/data/CounterRepositoryImpl.kt
+++ app/src/main/java/com/example/qrcodeapp/data/CounterRepositoryImpl.kt
@@ -8,11 +8,15 @@ class CounterRepositoryImpl(context: Context): CounterRepository {
     private val prefs = context.getSharedPreferences("counter_prefs", Context.MODE_PRIVATE)
 
     override fun getCounter(): Int {
-        return prefs.getInt("counter_key", 0)
+        return prefs.getInt(COUNTER_KEY, 0)
     }
 
     override fun updateCounter(value: Int): Int {
-        prefs.edit { putInt("counter_key", value) }
+        prefs.edit { putInt(COUNTER_KEY, value) }
         return value
+    }
+
+    companion object {
+        private const val COUNTER_KEY = "counter_key"
     }
 }
